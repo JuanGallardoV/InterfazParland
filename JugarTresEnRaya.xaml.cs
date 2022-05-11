67020a53cee2b7628173104b8f44bfe3553c3fb6
Juan Gallardo <juangallardovergara17@gmail.com>
2022-05-11 11:30:19 -0400

    Agregar archivos de proyecto.

---
+++ JugarTresEnRaya.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InterfazParland
+{
+    /// <summary>
+    /// Lógica de interacción para JugarTresEnRaya.xaml
+    /// </summary>
+    public partial class JugarTresEnRaya : Page
+    {
+        public JugarTresEnRaya()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalir_Click(object sender, RoutedEventArgs e)
+        {
+            App.ParentWindowRef.ParentFrame.Navigate(new MenuPrincipal());
+        }
+
+        private void btnMultijugador_Click(object sender, RoutedEventArgs e)
+        {
+            App.ParentWindowRef.ParentFrame.Navigate(new TresEnRayaMultijugador());
+        }
+    }
+}
